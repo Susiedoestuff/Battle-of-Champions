2217ce7a11264c6268eff6e388364cbfb9fb4e7f
Shuyan Qi <shuyanqi@mmc.school.nz>
2022-08-10 12:45:24 +1200

    Add project files.

---
+++ BrawlTest/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace BrawlTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.VeloX = new System.Windows.Forms.Label();
+            this.veloTmr = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.VeloX);
+            this.mainPanel.Location = new System.Drawing.Point(-1, -1);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(848, 680);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            // 
+            // VeloX
+            // 
+            this.VeloX.AutoSize = true;
+            this.VeloX.Location = new System.Drawing.Point(14, 14);
+            this.VeloX.Name = "VeloX";
+            this.VeloX.Size = new System.Drawing.Size(33, 13);
+            this.VeloX.TabIndex = 0;
+            this.VeloX.Text = "Velox";
+            // 
+            // veloTmr
+            // 
+            this.veloTmr.Enabled = true;
+            this.veloTmr.Interval = 10;
+            this.veloTmr.Tick += new System.EventHandler(this.veloTmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 677);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer veloTmr;
+        private System.Windows.Forms.Label VeloX;
+    }
+}
+
---
+++ BrawlTest/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrawlTest
+{
+    public partial class Form1 : Form
+    {
+        Rectangle Cedric, platform;
+        Graphics g; // declare the graphics object
+        SolidBrush greenbrush = new SolidBrush(Color.Purple);
+        int xv, yv, xa, ya;
+        bool falling, jumping;
+        //player stats
+        int hp, atk, def, dex, spd;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, mainPanel, new object[] { true });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cedric = new Rectangle(400, 400, 50, 50);
+            hp = 100; //how much damage they can take, ranges from 75 - 200
+            atk = 20; //how much base damage they can deal, ranges from 10 - 25
+            def = 4; //how much damage and knockback is negated, ranges from 2 - 10
+            dex = 5; //skill and hit recovery time, lower the better, ranges from 20 - 5
+            spd = 7; //base movement acceleration, ranges from 3 - 7
+            platform = new Rectangle(1, 600, 1000, 50);
+            mainPanel.Invalidate();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (xv < 10 && xv > -10)//velocity cap
+            {
+                if (e.KeyData == Keys.Right) { xa = spd; }
+                if (e.KeyData == Keys.Left) { xa = -spd; }
+            }
+            if (jumping == false)
+            {
+                if (e.KeyData == Keys.Up && yv < 50 && yv > -50 && falling == false)//check is players on a platform and velocity cap
+                {
+                    ya = -10;
+                    jumping = true;
+                }
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Right || e.KeyData == Keys.Left)
+            { xa = 0; }
+            if (e.KeyData == Keys.Up)
+            { ya = 0; jumping = false; }
+        }
+
+        private void mainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.FillRectangle(greenbrush, Cedric);
+            g.FillRectangle(greenbrush, platform);
+
+        }
+
+        private void veloTmr_Tick(object sender, EventArgs e)
+        {
+            mainPanel.Invalidate();
+            VeloX.Text = ya.ToString();
+            Cedric.Y += yv;
+            Cedric.X += xv;
+            
+            if (Cedric.IntersectsWith(platform))
+            {
+                if (falling == true)
+                {
+                    int place = (int)Cedric.Y;
+                    ya = 0;
+                    Cedric.Y -= place - 551;
+                    yv = 0;
+                    falling = false;
+                }
+            }
+            else 
+            {
+                ya += 1;
+                falling = true;
+            }
+            if (yv > 0)//y-velocity decay
+            {
+                yv -= 1;
+            }
+            if (yv < 0)
+            {
+                yv += 1;
+            }
+            if (yv < 20 && yv > -20)//velocity cap
+            {
+                yv += ya; //acceleration
+            }
+
+
+            if (xv < 10 && xv > -10)//velocity cap
+            { 
+                xv += xa; //acceleration
+            }
+            if (xv > 0)//coding for air resistance 
+            { 
+                xv -= 1;
+            }
+            if (xv < 0)
+            {
+                xv += 1;
+            }
+        }
+    }
+}
