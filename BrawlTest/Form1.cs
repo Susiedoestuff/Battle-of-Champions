3682d872ecb3f13a8430f1ca80f5e0db665307d0
Shuyan Qi <shuyanqi@mmc.school.nz>
2022-08-19 09:55:15 +1200

    Progress that was lost

--- BrawlTest/Form1.cs
+++ BrawlTest/Form1.cs
@@ -14,13 +14,20 @@ namespace BrawlTest
 {
     public partial class Form1 : Form
     {
-        Rectangle Cedric, platform;
+        Rectangle p1Sprite, p2Sprite, platform;
+        Rectangle p1hb, p1atkhb, p1spclhb; //hitboxes
+
         Graphics g; // declare the graphics object
         SolidBrush greenbrush = new SolidBrush(Color.Purple);
-        int xv, yv, xa, ya;
+        SolidBrush purplebrush = new SolidBrush(Color.Green);
+
+        int p1xv, p1yv, p1xa, p1ya;
+        int p1atkCooltime, p1spclCooltime; //cooldown timers
+        int p1hp, p1atk, p1def, p1dex, p1spd;
         bool falling, jumping;
-        //player stats
-        int hp, atk, def, dex, spd;
+        bool p1falling, p1jumping, p1atkActive, p1spclActive, p1invisFrame; //indicators if these things are active
+        bool p1atkCool, p1sklCool; //skill cooldowns
+        bool p1facingRight; //which side is the player facing
 
 
         public Form1()
@@ -31,21 +38,33 @@ namespace BrawlTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Cedric = new Rectangle(400, 400, 50, 50);
-            hp = 100; //how much damage they can take, ranges from 75 - 200
-            atk = 20; //how much base damage they can deal, ranges from 10 - 25
-            def = 4; //how much damage and knockback is negated, ranges from 2 - 10
-            dex = 5; //skill and hit recovery time, lower the better, ranges from 20 - 5
-            spd = 7; //base movement acceleration, ranges from 3 - 7
+            p1Sprite = new Rectangle(400, 400, 50, 50);
+            p1hp = 100; //how much damage they can take, ranges from 75 - 200
+            p1atk = 25; //how much base damage they can deal, ranges from 10 - 25
+            p1def = 4; //how much damage and knockback is negated, ranges from 2 - 10
+            p1dex = 5; //skill and hit recovery time, lower the better, ranges from 20 - 5
+            p1spd = 3; //base movement acceleration
+
+            //the same stuff for player 2
+            p2Sprite = new Rectangle(400, 400, 50, 50);
+            p2hp = 100;
+            p2atk = 25;
+            p2def = 4;
+            p2dex = 5;
+            p2spd = 3;
+
             platform = new Rectangle(1, 600, 1000, 50);
             mainPanel.Invalidate();
         }
+
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (xv < 10 && xv > -10)//velocity cap
+            //p1 movements
+            if (p1xv < 10 && p1xv > -10)//velocity cap
             {
-                if (e.KeyData == Keys.Right) { xa = spd; }
-                if (e.KeyData == Keys.Left) { xa = -spd; }
+                if (e.KeyData == Keys.Right) { p1xa = p1spd; p1facingRight = true; }
+                if (e.KeyData == Keys.Left) { p1xa = -p1spd; p1facingRight = false; }
             }
             if (jumping == false)
             {
@@ -55,6 +74,14 @@ namespace BrawlTest
                     jumping = true;
                 }
             }
+            if (p1atkCool == false && p1spclActive == false && p1atkActive == false) //if no skill is currently active
+            {
+                if (e.KeyData == Keys.Enter) { p1Atk(); } //actives attack skill
+            }
+
+
+
+
         }
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
@@ -72,20 +99,24 @@ namespace BrawlTest
 
         }
 
+
+        //----------------------------------\\
+        //             Movement             \\
+        //----------------------------------\\
         private void veloTmr_Tick(object sender, EventArgs e)
         {
             mainPanel.Invalidate();
             VeloX.Text = ya.ToString();
-            Cedric.Y += yv;
-            Cedric.X += xv;
+            p1hb.Y += yv;
+            p1hb.X += xv;
             
-            if (Cedric.IntersectsWith(platform))
+            if (p1hb.IntersectsWith(platform))
             {
                 if (falling == true)
                 {
-                    int place = (int)Cedric.Y;
+                    int place = (int)p1hb.Y;
                     ya = 0;
-                    Cedric.Y -= place - 551;
+                    p1hb.Y -= place - 551;
                     yv = 0;
                     falling = false;
                 }
@@ -122,5 +153,77 @@ namespace BrawlTest
                 xv += 1;
             }
         }
-    }
+
+
+
+
+
+
+
+
+
+        //----------------------------------\\
+        //          Player Skills           \\
+        //----------------------------------\\
+
+
+        private void p1Atk()
+        {
+            p1atkCool = true;
+            p1atkActive = true;
+            p1atkTime.Enabled = true;
+            p1atkCooltime = 0;
+            switch (p1Character)
+            {
+                case "Cedric":
+                    CedricAtk();
+                    break;
+                case "Riol":
+                    RiolAtk();
+                    break;
+            }
+        }
+        private void p1atkTime_Tick(object sender, EventArgs e) //tracks cooldown time
+        {
+            if (p1atkCooltime > p1dex)
+            {
+                p1atkTime.Enabled = false;
+                p1atkCool = false;
+            }
+            else
+            {
+                p1atkCooltime += 1;
+            }
+        }
+
+
+
+        //----------------------------------\\
+        //       Player Skills List         \\
+        //----------------------------------\\
+
+        //cool person :)
+        private void CedricAtk()
+        {
+            int playerposX, playerposY;
+            if (p1facingRight == true)
+            {
+
+                playerposX = p1Sprite.X + 130;
+                playerposY = p1Sprite.Y + 70;
+            }
+            else
+            {
+                playerposX = p1Sprite.X;
+                playerposY = p1Sprite.Y + 70;
+            }
+
+            p1atkhb = new Rectangle(playerposX, playerposY, 70, 40);
+        }
+
+        private void RiolAtk()
+        {
+        }
+
+        }
 }
